d7ff6c2d4977fb0d7ff713536fffa8f341835cc2
Tim Duffey <decoy31@hotmail.com>
2022-03-21 01:28:04 -0700

    Made some large updates to the homepage and moved some of the asset files around.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-//const defaultTheme = require('tailwindcss/defaultTheme');
+// const defaultTheme = require('tailwindcss/defaultTheme');
+const AspectRatio = require('@tailwindcss/aspect-ratio');
 
 module.exports = {
   content: [
@@ -6,20 +7,23 @@ module.exports = {
     './index.html'
   ],
   theme: {
-    container: {
-      center: true,
-    },
-    colors: {
-      'black': '#292929',
-      'orange': '#ffa33d',
-      'light-blue': '#15a1d4'
-    },
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: '2rem'
+      },
+      colors: {
+        'black': '#292929',
+        'orange': '#ffa33d',
+        'light-blue': '#15a1d4'
+      },
+      borderColor: '#333333'
+    }
   },
   plugins: [
     // The aspect-{ratio} utilities use the native aspect-ratio CSS property, which was not supported in Safari until
     // version 15. Until Safari 15 is popularized, Tailwind’s aspect-ratio plugin is a good alternative.
     // Source: https://tailwindcss.com/docs/aspect-ratio
-    require('@tailwindcss/aspect-ratio')
+    AspectRatio
   ],
 }
