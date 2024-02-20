97d409b9e8e2358c34e5ba2ca046d0e32856a7f9
Tim Duffey <decoy31@hotmail.com>
2024-02-20 15:29:08 -0800

    Added chat.js script.

---
+++ chat.js
@@ -0,0 +1,9 @@
+(function(n,i,_n,j,a){
+  n.addEventListener("DOMContentLoaded", function() {
+  i['liveninja'] = ["https://messenger.net2phone.com/w/65c257be554f0deb124306c5?frame=true"];
+  var _n = n.createElement('script');
+  var j = n.getElementsByTagName('script')[0];
+  _n.async = 1;
+  _n.src='https://messenger.net2phone.com/w/assets/widget.js';
+  j.parentNode.insertBefore(_n, j);
+})})(document, window);
